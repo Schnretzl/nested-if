2c850a67628252d2c5baac49402ecc42db48f98d
Schnretzl <wrhendley@gmail.com>
2024-08-19 17:19:46 -0500

    Add task 2

--- problem1.py
+++ problem1.py
@@ -1,6 +1,7 @@
 # 1. Nested Decisions: The Adventure Game
 
 # Task 1: Code Correction
+# Task 2: Setting the Scene
 place = input("Choose a place: forest or cave? ")
 
 if place == "forest":
@@ -12,4 +13,8 @@ if place == "forest":
     else:
         print("I don't know what you did.")
 elif place == "cave":
-    print("You find a hidden treasure!")
+    torch = input("Do you want to (light a torch) or (proceed in the dark)? ")
+    if torch == "light a torch":
+        print("Good idea.  Use a torch to light your way.")
+    elif torch == "proceed in the dark":
+        print("You can't see very well.  Proceed carefully.")
