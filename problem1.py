3a3f4797d52c029c1d17953db42edcd1222e8cb4
Schnretzl <wrhendley@gmail.com>
2024-08-19 17:22:13 -0500

    Add task 3

--- problem1.py
+++ problem1.py
@@ -2,6 +2,7 @@
 
 # Task 1: Code Correction
 # Task 2: Setting the Scene
+# Task 3: Default Path
 place = input("Choose a place: forest or cave? ")
 
 if place == "forest":
@@ -11,10 +12,14 @@ if place == "forest":
     elif action == "cross a river":
         print("You found a boat!")
     else:
-        print("I don't know what you did.")
+        pass
 elif place == "cave":
     torch = input("Do you want to (light a torch) or (proceed in the dark)? ")
     if torch == "light a torch":
         print("Good idea.  Use a torch to light your way.")
     elif torch == "proceed in the dark":
         print("You can't see very well.  Proceed carefully.")
+    else:
+        pass
+else:
+    pass
