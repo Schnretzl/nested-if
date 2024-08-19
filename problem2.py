2c5931e87c30c8b2a1b189f615192211f8b72de6
Schnretzl <wrhendley@gmail.com>
2024-08-19 17:25:05 -0500

    Add task 1

---
+++ problem2.py
@@ -0,0 +1,6 @@
+# 2. Quick Decisions: The Event Planner
+
+# Task 1: Code Correction
+attendees = int(input("Enter number of attendees: "))
+venue = "large hall" if attendees > 100 else "conference room"
+print(venue)
